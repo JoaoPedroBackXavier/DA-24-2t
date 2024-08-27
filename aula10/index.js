965bcc52888e065e604c1f6c62ee3b6be97a1e8d
JoaoPedroBackXavier <joaopedro.backxavier@gmail.com>
2024-08-27 17:21:45 -0300

    aula ten

---
+++ aula10/index.js
@@ -0,0 +1,111 @@
+// let condicao_1 = true
+
+// if(condicao_1){
+//     alert("oi user")
+// }
+
+// condicao_1 = false
+
+// if(condicao_1){
+//     alert("isso nao vai ser exibido")
+// }
+
+// ----------------------------
+
+// N1 = Number(prompt("digite o N1"))
+// N2 = Number(prompt("digite o N2"))
+
+// if(N1===N2){
+//     alert(`${N1} é igual á ${N2}`)
+// }else if(N1 > N2){
+//     alert(`${N1} é maior que ${N2}`)
+// }else{
+//     alert(`${N2} é maior que ${N1}`)
+// }
+
+// ------------------------------
+
+// let paisDeOrigem = prompt("qual seul pauis de origem")
+// switch (paisDeOrigem){
+//     case "Brasil" :
+//         alert("aqi é o brazil!")
+//         break
+//     case "eua" :
+//         alert("seu pais é eua")
+//         break
+//     default :
+//         alert("pais não listado")
+//     }
+
+// ---------------------------------
+
+// let pokemon = prompt("qual pokemon voce escolhe (Bulbassauro, Charmander, Squirtle")
+// switch (pokemon){
+//     case "Bulbassauro" :
+//         alert("Bulbassauro é tipo planta e veneno")
+//         break
+//     case "Charmander" :
+//         alert("Charmander é tipo fogo")
+//         break
+//     case "Squirtle" :
+//         alert("Squirtle é tipo agua")
+//         break
+//     default :
+//         alert("erro de digitação")
+//     }
+
+// -----------------------------------
+
+// let personDHarry = prompt("qual a aluna?")
+// switch (personDHarry){
+//     case "Hermione" :
+//         alert("Grifnoria")
+//         break
+//     case "Ana" :
+//         alert("lufa-lufa")
+//         break
+//     case "Luna" :
+//         alert("corvinal")
+//         break
+//     case "Narcisa" :
+//         alert("sonserina")
+//         break
+//     default :
+//         alert("chapel falhou")
+//     }
+
+// ------------------------------------------
+
+// let respostas =[
+//     prompt("ja terminou o ens. med.???"),
+//     prompt("tem mais de 17 anos???"),
+//     prompt("esta cursando outra facul???")
+// ]
+
+// cond1=true
+// cond2=true
+// cond3=true
+
+// if (respostas[0] == "sim"){
+//     cond1 = true
+//     }else{
+//     cond1 = false        
+//     }
+// if (respostas[1] == "sim"){
+//     cond2 = true
+//     }else{
+//     cond2 = false          
+//     }
+// if (respostas[2] == "sim"){
+//     cond3 = true
+//     }else{
+//     cond3 = false          
+//     }
+
+// if (cond1 && cond2 && (!cond3)){
+//     alert("aprovadissimo")
+// }else{
+//     alert("ta maluco meooooooooo")
+// }
+
+// ---------------------------------------------
