0f25afb819bc3dab95eebe1699c3c6c68be61596
JoaoPedroBackXavier <joaopedro.backxavier@gmail.com>
2024-09-10 16:40:17 -0300

    aula 7

---
+++ aulas/aula7/index.js
@@ -0,0 +1,110 @@
+// let N1 = 5
+// let N2 = 10
+// alert(N1 > N2)
+// alert(N1 < N2)
+// alert(N1 === N2)
+// alert(N1 !== N2)
+
+// let a = true
+// let b = true
+// let c = false
+
+// alert(a && b)
+// alert(a && b && c)
+// alert(c || a)
+// alert(!c)
+
+// exercicios de fix
+
+// 1.
+// a. false
+// b. false
+// c. true
+// d. boolean
+
+// 2.
+// Number()
+
+// 1.
+
+// let idade1 = Number(prompt("qual sua idade"))
+// let idade2 = Number(prompt("qual idade do seu friendo?"))
+// if(idade1 > idade2){
+//     alert("voce é mais alto")
+// }else if(idade2 > idade1){
+//     alert("voce é menor")
+// }
+
+// 2.
+
+// let idadeA = Number(prompt("qual sua idade em anos"))
+// let meses = idadeA * 12
+// let dias = meses * 30
+// alert(`sua idade é ${meses} meses e ${dias} dias`)
+
+// 3.
+// let N1 = Number(Prompt("digite N1"))
+// let N2 = Number(Prompt("digite N2"))
+// alert(`n1 e maior: ${N1 > N2} n2 é maior ${N2 > N1} é igual?: ${N1 == N2} é diferente?: ${N1 !== N2}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
