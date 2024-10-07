2cd7197492c2ace38367b62f95e77936868c81ac
JoaoPedroBackXavier <joaopedro.backxavier@gmail.com>
2024-10-07 16:17:29 -0300

    atualização

--- trabalhoAvaliativoC/index.js
+++ trabalhoAvaliativoC/index.js
@@ -1,14 +1,14 @@
 let personagens = [
-    {nome:"link",      coracoes:5 ,  forca:2, defesa:3},
-    {nome:"Ganondorf", coracoes:15, forca:8 , defesa:5},
-    {nome:"zelda",     coracoes:1 ,  forca:1, defesa:1}
+    {nome: "link",      coracoes:1 ,  forca:1, defesa:1},
+    {nome:"ganondorf", coracoes:15, forca:8 , defesa:5},
+    {nome:"zelda",     coracoes:1 ,  forca:1, defesa:1},
 ]
 // array de personagens
 
 let armas = [
     {nome:"master sword", tipo:"arma de uma mão",   dano:30, alcance:"curto"},
     {nome:"bow",          tipo:"arco" ,             dano:15, alcance:"longo"},
-    {nome:"hammer",       tipo:"arma de duas mãos", dano:20, alcance:"curto"}
+    {nome:"hammer",       tipo:"arma de duas mãos", dano:0, alcance:"curto"}
 ]
 // array de armas
 
@@ -30,7 +30,7 @@ function validarCoracoes(){
         let coracoesAux = personagem.coracoes
 // isso diminui a repetição de codigo
         if((coracoesAux>0)&&(coracoesAux<21)){}else{
-            mensagemDeErro(`numero de corações invalido no personagem ${personagem.nome}`)
+            mensagemDeErro(`numero de corações invalido`)
             return false
         }
     }
@@ -38,16 +38,47 @@ function validarCoracoes(){
 }
 // eu deichei o return false para funcionar como um break
 
+function validarNomes(){
+
+    let aux = true
+    for(personagem of personagens){
+        if((personagem.nome == "ganondorf")||(personagem.nome == "link")||(personagem.nome == "zelda")){
+
+        }else{
+            mensagemDeErro(`nome ${personagem.nome} esta errado`)
+            aux = false
+            return false
+        }
+    }
+    if (aux){
+        alert("nome dos personagens validos")
+    }
+}
+
 function validarArmas(){
     for(const arma of armas){
         if("nome" in arma){
 // verifica se arma tem propriedade nome
             if("dano" in arma){
+
                 if("alcance" in arma){
+
                     if(arma.dano > 0){
 // verifica se arma tem dano maior que 0
                         if((arma.nome == "master sword")||(arma.nome == "bow")||(arma.nome == "hammer")){
 
+                            if((arma.tipo == "arma de uma mão")||(arma.tipo == "arco")||(arma.tipo == "arma de duas mãos")){
+
+                                if((arma.alcance == "curto")||(arma.alcance == "longo")){
+
+                                }else{
+                                    mensagemDeErro(`a arma de alcance ${arma.alcance} tem alcance invalido`) 
+                                    return false 
+                                }
+                            }else{
+                                mensagemDeErro(`a arma de tipo ${arma.tipo} tem tipo invalido`) 
+                                return false 
+                            }
                         }else{
                             mensagemDeErro(`a arma de nome ${arma.nome} tem nome invalido`) 
                             return false }
@@ -93,13 +124,48 @@ function validarItens(){
                 break
 // verifica se o efeito é registrado
             default:
-                mensagemDeErro("nome de item invalido")
+                mensagemDeErro("descrição de item invalido")
         }
     }
     alert("itens validos")
 }
 
+function validarDefesa(){
+    let aux = true
+    for(personagem of personagens){
+        let defesa = personagens.defesa
+        if(defesa > 0){
+        }else{
+           aux = false
+        }
+    }
+    if (aux){
+        alert("defesa valida")
+    }else{
+        mensagemDeErro("defesa invalida")
+    }
+}
 
+function validarForca(){
+    let aux = true
+    for(personagem of personagens){
+        let forca = personagem.forca
+        if(forca > 0){
+        }else{
+            aux = false
+        }
+    }
+    if(aux){
+        alert("força valida")
+    }else{
+        mensagemDeErro("força invalida")
+    }
+}
+
+
+validarNomes()
 validarCoracoes()
+validarDefesa()
+validarForca()
 validarArmas()
 validarItens()
